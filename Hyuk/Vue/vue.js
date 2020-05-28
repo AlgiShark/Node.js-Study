722f4dbace5503bb8daed10ccf0b363849f3042c
DevLuce <whchoi000@gmail.com>
2020-05-28 18:15:28 +0900

    update Event Handling

--- Hyuk/Vue/vue.js
+++ Hyuk/Vue/vue.js
@@ -9,11 +9,22 @@ var app = new Vue({
             {
                 variantID: 2234,
                 variantColor: "green",
+                variantImage: "./assets/vmSocks-green.jpg",
             },
             {
                 variantID: 2235,
                 variantColor: "blue",
+                variantImage: './assets/vmSocks-blue.jpg',
             }
-        ]
+        ],
+        cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
     }
 })
