3700c50530f7081a4aeccc6230929004bbc2f0f1
DevLuce <whchoi000@gmail.com>
2021-01-12 23:19:36 +0900

    Studying : 리액트 ref, 비구조화 할당
    ## 공부한 기술 스택
    React.js, ref
    
    ## 공부한 세부 내용
    ref 를 사용한 DOM 접근, 일반 태그에서 ref 사용, 컴포넌트에서의 ref 사용, 비구조화 할당

--- Hyuk/React/React-Book/hello-react/src/App.js
+++ Hyuk/React/React-Book/hello-react/src/App.js
@@ -1,11 +1,22 @@
+import React, { Component } from 'react';
 import './App.css';
 import MyComponent from './MyComponent';
 import EventPractice from './EventPractice';
+import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
+import { render } from 'react-dom';
 
-function App() {
-  return (
-    <EventPractice/>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => this.scrollBox = ref} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
