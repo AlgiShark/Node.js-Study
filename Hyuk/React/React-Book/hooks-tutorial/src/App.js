47a85a1a507f0369a0aa9e4a07960c76e4748016
DevLuce <whchoi000@gmail.com>
2021-01-24 23:52:37 +0900

    Studying : 리액트, useReducer
    ## 공부한 기술 스택
    react.js, useReducer
    
    ## 공부한 세부 내용
    다양한 상태를 다양한 값으로 업데이트, name 속성을 사용해 여러 인풋 업데이트

--- Hyuk/React/React-Book/hooks-tutorial/src/App.js
+++ Hyuk/React/React-Book/hooks-tutorial/src/App.js
@@ -6,13 +6,7 @@ import React, { useState } from 'react';
 function App() {
   const [visible, setVisible] = useState(false);
   return (
-    <div>
-      <button onClick={() => { setVisible(!visible); }}>
-        {visible ? '숨기기' : '보이기'}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
+    <Info />
   );
 }
 
