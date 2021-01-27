3a999cf117365aebaa6e1924dab2fda9099ae8c6
DevLuce <whchoi000@gmail.com>
2021-01-27 23:57:47 +0900

    Studying : 리액트, useMemo
    ## 공부한 기술 스택
    react.js, useMemo
    
    ## 공부한 세부 내용
    useMemo를 이용한 불필요한 렌더링 요소 제거

--- Hyuk/React/React-Book/hooks-tutorial/src/App.js
+++ Hyuk/React/React-Book/hooks-tutorial/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Counter from './Counter';
 import Info from './Info';
+import Average from './Average';
 import React, { useState } from 'react';
 
 function App() {
   const [visible, setVisible] = useState(false);
   return (
-    <Info />
+    <Average />
   );
 }
 
